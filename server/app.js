11d5fe7f280c1ede2b136d2148a075ac37e8337a
Shubhanshu Trivedi <itsshubhanshu03@gmail.com>
2025-05-08 23:36:55 +0530

    Setup cors

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
+import cors from "cors";
 import connectDB from "./db/db.js";
 import authRoutes from "./routes/auth.route.js";
 
@@ -13,6 +14,14 @@ app.use(bodyParser.json());
 
 // Parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// Cors
+const corsOptions = {
+  origin: "http://localhost:5173",
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 
 // Routes
 app.use("/api/v1/auth", authRoutes);
