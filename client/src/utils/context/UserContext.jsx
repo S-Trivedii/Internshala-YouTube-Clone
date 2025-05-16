cbbadfea3f92ce9cc019b0d2c6daee7b34d0c73d
Shubhanshu Trivedi <itsshubhanshu03@gmail.com>
2025-05-16 20:11:02 +0530

    Add isLoggedIn for LoggedOutHome Page

--- client/src/utils/context/UserContext.jsx
+++ client/src/utils/context/UserContext.jsx
@@ -1,4 +1,6 @@
-import { createContext, useContext, useState } from "react";
+import axios from "axios";
+import { createContext, useContext, useEffect, useState } from "react";
+import { USER_API_END_POINT } from "../apiEndPoint";
 
 // First create a context with or without object.
 const UserContext = createContext();
@@ -6,9 +8,28 @@ const UserContext = createContext();
 // UserProvider component
 export const UserProvider = ({ children }) => {
   const [user, setUser] = useState(null);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const res = await axios.get(`${USER_API_END_POINT}/me`, {
+          withCredentials: true,
+        });
+
+        // console.log("User:- ", res.data);
+        setUser(res.data.user);
+        setIsLoggedIn(true);
+      } catch (error) {
+        setUser(null);
+      }
+    };
+
+    fetchUser();
+  }, []);
 
   return (
-    <UserContext.Provider value={{ user, setUser }}>
+    <UserContext.Provider value={{ user, setUser, isLoggedIn, setIsLoggedIn }}>
       {children}
     </UserContext.Provider>
   );
