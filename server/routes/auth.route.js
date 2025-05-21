028887236e95d317776e17c32deb25309adc0cd5
Shubhanshu Trivedi <itsshubhanshu03@gmail.com>
2025-05-21 17:52:43 +0530

    Add me route

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { register, login, logout } from "../controllers/auth.controller.js";
+import { register, login, logout, me } from "../controllers/auth.controller.js";
+import { verifyUser } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
 router.get("/logout", logout);
+router.get("/me", verifyUser, me);
 
 export default router;
