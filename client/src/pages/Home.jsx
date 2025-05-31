bd72e40ce273e005ce9ac6a47145c36206bbdc15
Shubhanshu Trivedi <itsshubhanshu03@gmail.com>
2025-05-31 10:56:26 +0530

    Added images for readme file

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import LoggedOutHome from "../components/LoggedOutHome";
 import VideoGrid from "../components/VideoGrid";
 import { useUser } from "../utils/context/UserContext";
 
+// Home
 const Home = () => {
   const { isLoggedIn } = useUser();
   return (
